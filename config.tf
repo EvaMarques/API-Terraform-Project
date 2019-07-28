89a3283439ecdd70f67081f820805fcbfcea259a
bagdfs <31652136+bagdfs@users.noreply.github.com>
2019-07-28 16:21:48 +0100

    modified tf with api gateway resource

--- config.tf
+++ config.tf
@@ -36,11 +36,13 @@ resource "aws_lambda_function" "random_numbers_lambda" {
 	runtime = "python2.7"
 }
 
-resource "aws_api_gateway_rest_api" "my_rest_api" {
-  name = "my-rest-api"
+resource "aws_api_gateway_rest_api" "MyDemoAPI" {
+  name        = "my-rest-api"
+  description = "This is my API for demonstration purposes"
 }
 
-resource "aws_api_gateway_resource" "my_resource" {
-  rest_api_id = "${data.aws_api_gateway_rest_api.my_rest_api.id}"
-  path        = "/api/generate"
+resource "aws_api_gateway_resource" "MyDemoResource" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyDemoAPI.id}"
+  parent_id   = "${aws_api_gateway_rest_api.MyDemoAPI.root_resource_id}"
+  path_part   = "mydemoresource"
 }
